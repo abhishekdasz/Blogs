65a36e1e6dcd1b2d2fb32ded0d3c8e6d0ecff134
Abhishek <abhishekdaszy@gmail.com>
2023-07-28 21:01:36 +0530

    fetch latest blogs

--- src/app/api/blogs/read/route.js
+++ src/app/api/blogs/read/route.js
@@ -11,7 +11,11 @@ export async function GET(req, res) {
     {
         return NextResponse.json({ success:false, message:'No blogs found' });
     }
-    return NextResponse.json({ status:200, success:true, message:'All blogs list', BlogsCount: blogs.length, blogs });
+    return NextResponse.json({ status:200, success:true, message:'All blogs list', BlogsCount: blogs.length, blogs }, {
+      headers: {
+        'Cache-Control': 'no-cache',
+      },
+    });
   } catch (e) {
     return NextResponse.json( { error: error.message }, { message: "Error while getting blogs!" }, { status: 500 });
   }
